63a51e06f7424447b48a1b27475108d58c2e2977
lerzi <569259131@qq.com>
2024-03-08 22:05:09 +0800

    feat: 404

--- src/views/404.vue
+++ src/views/404.vue
@@ -3,7 +3,16 @@
 </script>
 
 <template>
-  <div>
-    <h1>404</h1>
-  </div>
+  <n-card h-full>
+    <div h-full flex justify-center pt-30>
+      <n-result status="404" title="404 资源不存在" description="生活总归带点荒谬">
+        <template #footer>
+          <router-link to="/">
+            <n-button type="primary">回到首页</n-button>
+          </router-link>
+        </template>
+      </n-result>
+    </div>
+  </n-card>
+
 </template>
